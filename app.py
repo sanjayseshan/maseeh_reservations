1e3c2a587965e183c018b1a8568bad1ef2180513
Sanjay Seshan <sanjay.seshan@outlook.com>
2025-04-10 23:45:32 -0400

    touchstone2

--- app.py
+++ app.py
@@ -163,9 +163,9 @@ def index():
 
     db = get_db()
     accounts = [(y,z) for x,y,z in db.execute("SELECT * FROM users").fetchall()]
-    print(accounts)
+    # print(accounts)
 
-    if (("loggedin" not in session or session["loggedin"] == False) and g.oidc_user.logged_in) or (g.oidc_user.profile.get('email').split("@mit.edu")[0] != session["user"]):
+    if (("loggedin" not in session or session["loggedin"] == False) and g.oidc_user.logged_in):
         session["user_name"] = g.oidc_user.profile.get('email').split("@mit.edu")[0]
         session["user"] = session["user_name"]
         session["password"] = "KERBEROS CERTIFICATE AUTHENTICATION"
